cb47aeeb2f75d2ae6fa4ecbfcb81ce8190e3052e
RichardJohansson501 <richardjohansson501@hotmail.com>
2017-08-10 16:29:24 +0200

    Primitiv GetWeekPeriod

--- LMS/Controllers/StudentsController.cs
+++ LMS/Controllers/StudentsController.cs
@@ -7,14 +7,55 @@ using System.Web.Mvc;
 
 namespace LMS.Controllers
 {
+    public struct PeriodData
+    {
+        public int Year;
+        public int Week;
+        public DateTime Start;
+        public DateTime End;
+    }
+
     public class StudentsController : Controller
     {
+        private DateTime GetMonday(int year, int week)
+        {
+            /// Converts a week number to corresponding monday date.
+            /// Note: Week 1 of a year may start in the previous year.
+            /// ISO 8601 week 1 is the week that contains the first Thursday that year.
+            /// If December 28 is a Monday, December 31 is a Thursday and week 1 starts January 4.
+            /// If December 28 is a Sunday, December 31 is a Wednesday and week 1 starts December 29 previous year.
+            var dec28 = new DateTime (year - 1, 12, 28);
+            var dec28DayOffset = ((int)dec28.DayOfWeek + 6) % 7;    // monday = 0, tuesday = 1, etc
+            return dec28.AddDays(7 * week - dec28DayOffset);
+        }
+
+        private PeriodData GetWeekPeriod(int year, int week, int moveWeek)
+        {
+            PeriodData periodData = new PeriodData();
+            periodData.Year = year;
+            periodData.Week = week + moveWeek;
+
+            periodData.Start = GetMonday(year, week);
+            periodData.End = periodData.Start.AddDays(4);
+
+            return periodData;
+        }
+
         // GET: Students/Scheme
-        public ActionResult Scheme(int courseId, int year, int week, int moveWeek)
+        public ActionResult Scheme(int? courseId, int? year, int? week, int? moveWeek)
         {
             SchemeViewModel viewModel = new SchemeViewModel();
-            viewModel.Year = year + 1;
-            viewModel.Week = week + moveWeek;
+            if (courseId == null)
+            {
+                year = 2017;
+                week = 10;
+                moveWeek = 0;
+            }
+            viewModel.Year = (int) year + 1;
+            viewModel.Week = (int) week + (int) moveWeek;
+
+            var periodData = GetWeekPeriod((int)year, (int)week, (int)moveWeek);
+            viewModel.Monday = periodData.Start;
             viewModel.Period = "2017-04-10 -- 2017-04-15";
             viewModel.WeekActivities = new List<SchemeActivity>();
 
