7a7a5f0164391e2f3d19383fe10c0e5385ce7f24
RichardJohansson501 <richardjohansson501@hotmail.com>
2017-08-17 23:41:28 +0100

    Generic HeadBanner handling

---
+++ LMS/ViewModels/CommonLOViewModels.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LMS.ViewModels
+{
+    public class HeadBannerViewModel
+    {
+        public string Type { get; set; }
+
+        public int Id1 { get; set; }
+        public int Id2 { get; set; }
+        public int Id3 { get; set; }
+
+        public string LinkText0 { get; set; }
+        public string LinkText1 { get; set; }
+        public string LinkText2 { get; set; }
+        public string LinkText3 { get; set; }
+
+        public string InfoText { get; set; }
+    }
+}
