184d79af580aabdc0246f0c56bc84ede67e1e58f
RichardJohansson501 <richardjohansson501@hotmail.com>
2017-08-18 08:47:16 +0200

    CommonLOs

--- LMS/Controllers/CommonLOsController.cs
+++ LMS/Controllers/CommonLOsController.cs
@@ -10,18 +10,18 @@ namespace LMS.Controllers
     public class CommonLOsController : Controller
     {
         // GET: CommonLOs
-        public ActionResult HeadBanner(string type, int? id1, int? id2, int? id3, string linkText0, string linkText1, string linkText2, string linkText3, string infoText)
+        public ActionResult HeadBanner(string origin, int? id1, int? id2, int? id3, string linkText0, string linkText1, string linkText2, string linkText3, string infoText)
         {
             HeadBannerViewModel viewModel = new HeadBannerViewModel();
-            viewModel.Type = type;
+            viewModel.Origin = origin;
             viewModel.Id1 = (id1 == null ? 0 : (int)id1);       // 0 indicates not used
             viewModel.Id2 = (id2 == null ? 0 : (int)id2);       // 0 indicates not used       
             viewModel.Id3 = (id3 == null ? 0 : (int)id3);       // 0 indicates not used
 
-            viewModel.LinkText0 = linkText0;
-            viewModel.LinkText1 = linkText1;
-            viewModel.LinkText2 = linkText2;
-            viewModel.LinkText3 = linkText3;
+            //viewModel.LinkText0 = linkText0;
+            //viewModel.LinkText1 = linkText1;
+            //viewModel.LinkText2 = linkText2;
+            //viewModel.LinkText3 = linkText3;
             viewModel.InfoText = infoText;
 
             return PartialView(viewModel);           
--- LMS/ViewModels/CommonLOViewModels.cs
+++ LMS/ViewModels/CommonLOViewModels.cs
@@ -7,16 +7,16 @@ namespace LMS.ViewModels
 {
     public class HeadBannerViewModel
     {
-        public string Type { get; set; }
+        public string Origin { get; set; }
 
         public int Id1 { get; set; }
         public int Id2 { get; set; }
         public int Id3 { get; set; }
 
-        public string LinkText0 { get; set; }
-        public string LinkText1 { get; set; }
-        public string LinkText2 { get; set; }
-        public string LinkText3 { get; set; }
+        //public string LinkText0 { get; set; }
+        //public string LinkText1 { get; set; }
+        //public string LinkText2 { get; set; }
+        //public string LinkText3 { get; set; }
 
         public string InfoText { get; set; }
     }
